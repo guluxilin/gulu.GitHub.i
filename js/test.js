f882f85e0f818ba2fb89abb5e3c4f71d25c53746
guluxilin <1215201453@qq.com>
2022-01-02 11:17:28 +0800

    第十二次提交

--- js/test.js
+++ js/test.js
@@ -1,12 +1,12 @@
 
 $.ajax({
-    url: "data.json", //json文件位置
+    url: "/data.json", //json文件位置
     type: "GET", //请求方式为get
     dataType: "json", //返回数据格式为json
     success: function(data) { //请求成功完成后要执行的方法
         //each循环 使用$.each方法遍历返回的数据date
 
-        path = `table.tpl`;
+        path = `/table.tpl`;
         domid = '#form_test';
         $.get(path, function(tpl){
             // 这里使用了链接调用,可以节省一个本地变量
